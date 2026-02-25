8951b92f0491afd0409154380c71c465b95995fe
GonzaloBernalll <gonzalo.bernal@live.u-tad.com>
2026-02-25 23:23:20 +0100

    Enemigo base prefab y el enemigo salto el cual tengo que arreglar

---
+++ Assets/Scripts/Enemigo/Enemigo Salto.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movimiento2D : MonoBehaviour
+{
+    public float velocidad = 8f;
+    public float fuerzaSalto = 15f;
+
+    private Rigidbody2D rb;
+    private float movimiento;
+    private bool enSuelo;
+
+    public Transform puntoSuelo;
+    public float radioSuelo = 0.2f;
+    public LayerMask capaSuelo;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Movimiento horizontal
+        movimiento = Input.GetAxisRaw("Horizontal");
+
+        // Comprobaci�n suelo
+        enSuelo = Physics2D.OverlapCircle(puntoSuelo.position, radioSuelo, capaSuelo);
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && enSuelo)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, fuerzaSalto);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
+    }
+}
